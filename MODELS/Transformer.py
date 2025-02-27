7101d5a94a5c1ec9c612ce3bf776238c84fb62b2
Nabi0-0 <vedanshi0@gmail.com>
2025-02-27 14:54:07 +0530

    fixing the extra argument issue

--- MODELS/Transformer.py
+++ MODELS/Transformer.py
@@ -8,8 +8,8 @@ class Transformer(nn.Module):
     def __init__(self, input_vocab_size, target_vocab_size, d_model, num_heads, d_ff, num_layers, dropout):
         super(Transformer, self).__init__()
 
-        self.encoder = EncoderBlock(input_vocab_size, d_model, num_heads, d_ff, num_layers, dropout)
-        self.decoder = DecoderBlock(target_vocab_size, d_model, num_heads, d_ff, num_layers, dropout)
+        self.encoder = EncoderBlock(d_model, num_heads, d_ff, dropout)        
+        self.decoder = DecoderBlock(d_model, num_heads, d_ff, dropout)        
         self.fc_out = nn.Linear(d_model, target_vocab_size)  # Final output layer
 
     def forward(self, src, trg, src_mask, trg_mask):
