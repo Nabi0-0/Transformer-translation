a56350345fee577e588025b156470d540668a329
Nabi0-0 <vedanshi0@gmail.com>
2025-02-27 11:49:08 +0530

    add positional encoding and text scripts

---
+++ MODELS/test_positional_Encoding.py
@@ -0,0 +1,20 @@
+#for testing postional encoding
+
+import torch
+from positional_encoding import PositionalEncoding
+
+#define model size and sequence length
+d_model = 512
+seq_len = 10
+
+#initializa positional encoding
+pos_enc = PositionalEncoding(d_model)
+
+#create a dummy input tensor of shape (batch size = 1, seq_len = 10, d_model = 512)
+x = torch.zeros(1, seq_len,d_model)
+
+#pass it through positional encoding
+encoded_x = pos_enc(x)
+
+#print output spape
+print("output shape: ", encoded_x.shape) #should be (1, 10, 512)
